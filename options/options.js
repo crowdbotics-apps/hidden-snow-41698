27560039ea17758cdb42733ec8034cbc0b035979
Crowdbotics <team@crowdbotics.com>
2023-06-23 15:53:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-snow-41698",
+  url: "https://hidden-snow-41698.botics.co",
+  api: "https://hidden-snow-41698.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
